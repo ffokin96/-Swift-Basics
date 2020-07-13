acb60fdf8776f8d2f827789cc111dd029c655fa9
MaximTimokhin <ffoka96@icloud.com>
2020-07-13 18:29:53 +0300

    First commit. empty file

---
+++ L1_TimikhinMaxim/L1_TimikhinMaxim/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L1_TimikhinMaxim
+//
+//  Created by Максим Тимохин on 13.07.2020.
+//  Copyright © 2020 Максим Тимохин. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
